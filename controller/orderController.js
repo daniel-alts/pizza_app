ed4140dec1fc14e3fb52681bb5bba51286d24a00
Opeyemi <opeadetola@gmail.com>
2022-09-22 14:25:10 -0400

    Updated order file in controller for CRUD operation

--- controller/orderController.js
+++ controller/orderController.js
@@ -13,6 +13,19 @@ exports.all_orders = (req, res) => {
     });
 };
 
+//GET orders by ID
+exports.order_by_id = (req, res) => {
+  const id = req.params.id;
+  orders
+    .findById(id)
+    .then((order) => {
+      res.status(200).send(order);
+    })
+    .catch(() => {
+      res.status(400).send({ message: "Order not found" });
+    });
+};
+
 //CREATE Orders
 exports.create_order = (req, res) => {
   const order = req.body;
@@ -33,6 +46,34 @@ exports.create_order = (req, res) => {
         message: "Your Order has been created Successfully",
         body: order,
       });
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
+};
+
+//UPDATE Orders
+exports.update_order = (req, res) => {
+  const id = req.params;
+  const state = req.body;
+
+  orders
+    .findByIdAndUpdate(id, state, { new: true })
+    .then((order) => {
+      res.status(200).send(order);
+    })
+    .catch(() => {
+      res.status(400).send(err.message);
+    });
+};
+
+//DELETE Orders
+exports.delete_order = (req, res) => {
+  const id = req.params.id;
+  orders
+    .findByIdAndDelete(id)
+    .then((order) => {
+      res.status(200).send({ message: "Order deleted Successfully" });
     })
     .catch((err) => {
       res.status(400).send(err.message);
