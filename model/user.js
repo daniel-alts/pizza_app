1a2ffc2e0d80f2b6602b4e6d7877a0b523e89cfe
Opeyemi <opeadetola@gmail.com>
2022-09-22 18:24:55 -0400

    Added bcrypt function to hash password in user schema

--- model/user.js
+++ model/user.js
@@ -1,11 +1,26 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   user_type: { type: String, enum: ["admin", "user"] },
+});
+
+//User Password hash with bcrypt
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model("users", userSchema);
