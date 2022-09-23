7b88681012f7b9f8b302c4384a4eb0ab6d4bd13a
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-09-23 04:20:08 +0100

    route: create user

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,18 @@ const userSchema = require("../models/userModel");
 
 const userRoute = express.Router();
 
+// Create user 
+userRoute.post("/", (req, res) => {
+  const user = req.body;
 
+  userSchema.create(user).then((user) => {
+    return res.status(201).json({
+      message: "User created successfully",
+      data: user,
+    });
+  }).catch((err) => {
+    return res.status(500).json(err.message);
+  });
+});
 
 module.exports = { userRoute };
