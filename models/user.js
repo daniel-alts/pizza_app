ae3704b526ca795b18ce128a97ab402ca0e50735
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-09-23 14:49:25 +0100

    Added user schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const {isEmail} = require("validator").default;
+
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: {
+        type: String,
+        validate: {
+            validator: isEmail,
+            message: (props) => {
+                return `${props.value} is not a valid email`;
+            },
+        },
+    },
+    user_type: {
+        type: String,
+        required: true,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    // Orders made by this user
+    orders: [String]
+});
+
+userSchema.virtual("fullName")
+    .get(function() {
+        return this.firstName + " " + this.lastName
+    })
+    .set(function(v) {
+        [this.firstName, this.lastName] = v.split(" ")
+    })
+
+
+const User = model("User", userSchema)
+
+
+module.exports = User
