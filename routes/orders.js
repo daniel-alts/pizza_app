4713d97b59d29a0ad99846c75e281070b87934dd
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-09-24 18:47:17 +0100

    created order route

---
+++ routes/orders.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+// import controllers
+const {createOrder, getSingleOrder, getAllOrders, updateOrder,deleteOrder } = require('../controller/order')
+
+router.route('/order').post(createOrder)
+
+router.route("/orders").get(getAllOrders)
+
+router.route('/order/:orderId').get(getSingleOrder).patch(updateOrder).delete(deleteOrder)
+
+
+module.exports = router
