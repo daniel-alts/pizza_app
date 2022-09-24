5b6b16dd260fde59ae4b7d6185304cb624d578a1
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-09-24 19:15:51 +0100

    Fixed authentication

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const orderModel = require('./models/order');
+const express = require("express");
+const mongoose = require("mongoose");
+const orderModel = require("./models/order");
 
-const orderRouter = require("./routes/order")
-const userRouter = require("./routes/user")
+const orderRouter = require("./routes/order");
+const userRouter = require("./routes/user");
 
-const authenticate = require("./auth")
-const { connectDB } = require("./db")
+const authenticate = require("./auth");
+const { connectDB } = require("./db");
 
-const PORT = 3334
+const PORT = 3334;
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    return res.json({ status: true })
-})
+app.get("/", (req, res) => {
+    return res.json({ status: true });
+});
 
 // Handle order routes
-app.use("/order", (req, res, next) => {
-    // Make sure user is authenticated
-    const user = req.body.user
-    authenticate(user).then(next()).catch(
-        err => res.status(401).end(err)
-    )
-
-}, orderRouter)
+app.use(
+    "/order",
+    async (req, res, next) => {
+        // Make sure user is authenticated
+        const user = req.body.user;
+        let authenticated = false;
+        let error;
+        await authenticate(user)
+            .then(() => (authenticated = true))
+            .catch((err) => (error = err));
+            
+        if (authenticated) next()
+        else {
+            if (error.message === "Incomplete query") res.status(400).send(error.toString())
+            else res.status(401).send(error.toString())
+        }
+    },
+    orderRouter
+);
 
 // Handle user routes
-app.use("/user", userRouter)
+app.use("/user", userRouter);
 
 app.get((req, res) => {
-    res.status(404).json({error: "Route not found"})
-})
+    res.status(404).json({ error: "Route not found" });
+});
 
 // Connect to mongo atlas instance
-connectDB()
+connectDB();
 
 app.listen(PORT, () => {
-    console.log('Listening on port, ', PORT)
-})
+    console.log("Listening on port, ", PORT);
+});
