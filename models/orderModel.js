86165e32efefb367e23cdf73f3b62ba86299dfca
Tobi Balogun <tobi.balo@gmail.com>
2022-09-25 11:08:56 +0100

    misc: update model schema

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const Schema = mongoose.Schema
 
 const OrderSchema = new Schema({
-  id: ObjectId,
-  created_at: Date,
+  created_at: { type: Date, default: new Date() },
   state: { type: Number, default: 1 },
   total_price: Number,
-  items: [{
-    name: String,
-    price: Number,
-    size: { type: String, enum: ['m', 's', 'l']},
-    quantity: Number,
-  }]
-});
+  items: [
+    {
+      name: String,
+      price: Number,
+      size: { type: String, enum: ['m', 's', 'l'] },
+      quantity: Number,
+    },
+  ],
+})
 
-const Order = mongoose.model('Order', OrderSchema);
+/**
+ * Convert id to string
+ * Remove id object from response
+ * Remove _v from response
+ */
+OrderSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-module.exports = Order;
+module.exports = mongoose.model('Order', OrderSchema)
