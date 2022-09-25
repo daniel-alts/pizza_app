9d7f1ef774a2d764af3f38bf53e1af26cfd03be8
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-25 18:36:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const { app, http } = require('./index');
+const { app } = require('./index');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+const server = require('./serverRoom');
+const server2 = require('./app');
 
 mongoose
 	.connect(process.env.MONGO_URL)
 	.then(console.log('Mongo Connected'))
 	.catch((err) => console.log(err));
-// console.log(app);
-const port = process.env.PORT || 9000;
 
-http.createServer(app).listen(port, () => {
-	console.log(`Node   ${port}`);
-});
+// console.log(app);
+// const port = process.env.PORT || 9000;
+
+const newServer = new server(server2, 5555);
+const Gat = new server(app, 9000);
+Gat.listen();
+newServer.listen();
