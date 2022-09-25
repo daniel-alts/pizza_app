e14103e0ccabe931e7ff8197ca1d0c79e90bbd31
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-25 02:19:44 +0100

    Update usersControllers.js

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,1 +1,29 @@
-const userModel = require('../Models/');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		profilePicture: {
+			type: String,
+			default: '',
+		},
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model('User', UserSchema);
