0178af82316ed79362839f3edffafea4de371512
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-25 04:14:43 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const Users = require('../Models/userModels');
 const router = require('express').Router();
+const AppError = require('../utils/AppError');
 
 // global user variable
 // !be careful
@@ -7,15 +8,37 @@ const router = require('express').Router();
 let user;
 
 // register
-exports.Register = async (req, res) => {
-	user = await Users.create({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password,
-	});
+exports.Register = async (req, res, next) => {
+	try {
+		user = await Users.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password,
+		});
+		res.status(201).json({
+			status: 'success',
+			data: user,
+		});
+	} catch (error) {
+		return next(new AppError(error.message, 404));
+	}
+};
 
-	res.status(201).json({
-		status: 'success',
-		data: user,
-	});
+exports.Login = async (req, res, next) => {
+	try {
+		user = await Users.findOne({
+			username: req.body.username,
+		});
+
+		if (user.password !== req.body.password) {
+			return next(new AppError(' Wrong Credentials', 403));
+		}
+
+		res.status(201).json({
+			status: 'success',
+			data: user,
+		});
+	} catch (error) {
+		return next(new AppError(error.message, 401));
+	}
 };
