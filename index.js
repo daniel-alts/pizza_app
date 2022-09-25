9cc68d249f1edc077a133e54969f785288fbe80e
Tobi Balogun <tobi.balo@gmail.com>
2022-09-25 14:00:24 +0100

    func: implement error handling middleware

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/order', async (req, res) => {
 /**
  * Get order by id
  */
-app.get('/order/:orderId', async (req, res) => {
+app.get('/order/:orderId', async (req, res, next) => {
   const { orderId } = req.params
   try {
     const order = await orderModel.findById(orderId)
@@ -51,78 +51,113 @@ app.get('/order/:orderId', async (req, res) => {
     }
     return res.json({ status: true, order })
   } catch (err) {
-    console.log(err)
+    next(err)
   }
 })
 
 /**
  * Get all orders
  */
-app.get('/orders', async (req, res) => {
-  const orders = await orderModel.find({})
+app.get('/orders', async (req, res, next) => {
+  try {
+    const orders = await orderModel.find({})
 
-  return res.json({ status: true, orders })
+    return res.json({ status: true, orders })
+  } catch (err) {
+    next(err)
+  }
 })
 
 /**
  * Get information about all orders
  */
-app.get('/orders/info', async (req, res) => {
-  const orders = await orderModel.find({})
-  const resObj = {}
-  resObj.numberOfOrders = orders.length
-  resObj.states = orders.reduce((obj, x) => {
-    if (!obj[x.state]) obj[x.state] = 0
-    obj[x.state] ++
-    return obj
-  }, {})
-  return res.json({status: true, data: resObj})
+app.get('/orders/info', async (req, res, next) => {
+  try {
+    const orders = await orderModel.find({})
+    const resObj = {}
+    resObj.numberOfOrders = orders.length
+    resObj.states = orders.reduce((obj, x) => {
+      if (!obj[x.state]) obj[x.state] = 0
+      obj[x.state]++
+      return obj
+    }, {})
+    return res.json({ status: true, data: resObj })
+  } catch (err) {
+    next(err)
+  }
 })
 
 /**
  * Update order state
  */
-app.patch('/order/:id', async (req, res) => {
+app.patch('/order/:id', async (req, res, next) => {
   const { id } = req.params
   const { state } = req.body
 
-  const order = await orderModel.findById(id)
+  try {
+    const order = await orderModel.findById(id)
 
-  if (!order) {
-    return res.status(404).json({ status: false, order: null })
+    if (!order) {
+      return res.status(404).json({ status: false, order: null })
+    }
+
+    if (state < order.state) {
+      return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
+    }
+
+    order.state = state
+
+    await order.save()
+
+    return res.json({ status: true, order })
+  } catch (err) {
+    next(err)
   }
-
-  if (state < order.state) {
-    return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
-  }
-
-  order.state = state
-
-  await order.save()
-
-  return res.json({ status: true, order })
 })
-
 
 /**
- * Delete order 
+ * Delete order
  */
-app.delete('/order/:id', async (req, res) => {
+app.delete('/order/:id', async (req, res, next) => {
   const { id } = req.params
-  const order = await orderModel.findOne({ _id: id })
-  const deleted = await order.remove()
-  return res.json({ status: true, deleted })
+  try {
+    const order = await orderModel.findOne({ _id: id })
+    const deleted = await order.remove()
+    if (deleted) {
+      return res.status(204).json({ status: true })
+    }
+  } catch (err) {
+    next(err)
+  }
 })
+
+/**
+ * Error handler middleware
+ */
+const errorHandler = (error, req, res, next) => {
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Unable to lookup order with provided ID' })
+  }
+
+  return res.status(500).send({ error: 'Sorry, error occured' })
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 /**
  * Connect to database
  */
 const URL = process.env.URI
-mongoose.connect(URL).then(() => {
-  console.log(`Connection to MongoDB successful`)
-}).catch(err => {
-  console.log(`Connection to MongoDB failed`, err.message)
-})
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log(`Connection to MongoDB successful`)
+  })
+  .catch((err) => {
+    console.log(`Connection to MongoDB failed`, err.message)
+  })
 
 /**
  * Start server
