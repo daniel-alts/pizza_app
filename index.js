d700ef247670dc2be14715ebd3cd9321d7af77a1
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-25 02:19:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const express = require('express');
 const ORDERROUTES = require('./routes/orderRoutes');
+const AUTH = require('./routes/authRoutes');
 const morgan = require('morgan');
+const AppError = require('./utils/AppError');
 const app = express();
 app.use(express.json());
 app.use(morgan('common'));
 
 app.use('/api/orders', ORDERROUTES);
+app.use('/api/', AUTH);
 
-// app.use();
+app.all('*', (req, res, next) => {
+	next(
+		new AppError(
+			`Can't find ${req.originalUrl} on this server!!`,
+			404,
+		),
+	);
+});
+
+app.use = (err, req, res, next) => {};
 module.exports = { app, http };
