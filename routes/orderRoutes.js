fe8fa4dfca8600804a4f200000c105cd2fd1e3f1
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-09-25 23:50:49 +0100

    refactored controllers for order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,163 +1,30 @@
 const express = require("express");
 
-const orderModel = require("../models/orderModel");
 const { authenticateHandler, adminAuthorizeHandler } = require("../auth");
+const {
+  createOrder,
+  updateOrder,
+  deleteOrder,
+  getAllOrders,
+  getOrder,
+} = require("../controllers/orderControllers");
 
 const router = express.Router();
 
+// Authenticate user
 router.use(authenticateHandler);
 
-router.post("/", async (req, res) => {
-  try {
-    const { order: body } = req.body;
-    const total_price = body.items?.reduce((prev, curr) => {
-      prev += curr.price;
-      return prev;
-    }, 0);
+router.post("/", createOrder);
 
-    const order = await orderModel.create({
-      items: body.items,
-      total_price,
-    });
-
-    return res.status(201).json({
-      success: true,
-      data: {
-        order,
-      },
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error,
-    });
-  }
-});
-
+// Authorize Admin
 router.use(adminAuthorizeHandler);
 
-router.get("/", async (req, res) => {
-  try {
-    const orders = await orderModel.find({});
-    return res.status(200).json({
-      success: true,
-      results: orders.length,
-      data: {
-        orders,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error,
-    });
-  }
-});
+router.get("/", getAllOrders);
 
-router.get("/:orderId", async (req, res) => {
-  try {
-    const { orderId } = req.params;
-    const order = await orderModel.findById(orderId);
+router.get("/:orderId", getOrder);
 
-    if (!order) {
-      return res.status(404).json({
-        success: false,
-        message: "Order not found!",
-      });
-    }
+router.patch("/:orderId", updateOrder);
 
-    return res.status(200).json({
-      success: true,
-      data: {
-        order,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error,
-    });
-  }
-});
-
-router.patch("/:orderId", async (req, res) => {
-  try {
-    const { orderId } = req.params;
-    const { order: body } = req.body;
-    if (!body) {
-      return res.status(400).json({
-        success: false,
-        message: "Bad request!",
-      });
-    }
-    const { state } = body;
-
-    const order = await orderModel.findById(orderId);
-
-    if (!order) {
-      return res.status(404).json({
-        success: false,
-        message: "Order not found!",
-      });
-    }
-
-    if (!state) {
-      return res.status(400).json({
-        success: false,
-        message: "Bad request! Only the state can be modified!",
-      });
-    }
-
-    if (state < order.state) {
-      return res.status(422).json({
-        success: false,
-        message: "Invalid operation!",
-      });
-    }
-
-    order.state = state;
-
-    await order.save();
-
-    return res.status(200).json({
-      success: true,
-      data: {
-        order,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error,
-    });
-  }
-});
-
-router.delete("/:orderId", async (req, res) => {
-  try {
-    const { orderId } = req.params;
-
-    const order = await orderModel.findById(orderId);
-
-    if (!order) {
-      return res.status(404).json({
-        success: false,
-        message: "Order not found!",
-      });
-    }
-
-    await orderModel.deleteOne({ _id: orderId });
-
-    return res.status(204).json({
-      success: true,
-      data: null,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error,
-    });
-  }
-});
+router.delete("/:orderId", deleteOrder);
 
 module.exports = router;
