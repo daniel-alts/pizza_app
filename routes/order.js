27322c0b39210448a080a113c9d7c4ec324e7534
Eleos <Peaceaanu247@gmail.com>
2022-09-29 22:01:29 +0100

    Protected the route with basic and ACL authentication

--- routes/order.js
+++ routes/order.js
@@ -2,11 +2,12 @@ const express = require("express")
 const orderRouter = express.Router()
 
 const orderController = require("../controllers/orderController")
+const { passwordAuth, adminAuth, userAuth } = require("../middleware/authMiddleware")
 
-orderRouter.post('/createOrder', orderController.order)
-orderRouter.get('/:orderId', orderController.getOrderById)
-orderRouter.get('/', orderController.getAllOrder)
-orderRouter.patch('/:id', orderController.updateOrder)
-orderRouter.delete('/:id', orderController.deleteOrder)
+orderRouter.post('/createOrder', [passwordAuth, userAuth], orderController.order)
+orderRouter.get('/:orderId', [passwordAuth, adminAuth], orderController.getOrderById)
+orderRouter.get('/', [passwordAuth, adminAuth], orderController.getAllOrder)
+orderRouter.patch('/:id', [passwordAuth, adminAuth], orderController.updateOrder)
+orderRouter.delete('/:id', [passwordAuth, adminAuth], orderController.deleteOrder)
 
 module.exports = orderRouter
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,12 @@ const express = require('express')
 const UserRouter = express.Router()
 
 const userController = require("../controllers/userController")
+const { passwordAuth, adminAuth, userAuth } = require("../middleware/authMiddleware")
+
 
 UserRouter.post('/signup', userController.signup)
-UserRouter.get('/', userController.allUsers)
-UserRouter.patch('/:id', userController.updateUser)
-UserRouter.delete('/:id', userController.deleteUser)
+UserRouter.get('/', [passwordAuth, adminAuth], userController.allUsers)
+UserRouter.patch('/:id', [passwordAuth], userController.updateUser)
+UserRouter.delete('/:id',[passwordAuth, adminAuth], userController.deleteUser)
 
 module.exports = UserRouter
