32ebc34685d6a5a981c11d60de2fb697dcf4d9db
Eleos <Peaceaanu247@gmail.com>
2022-09-30 00:10:18 +0100

    Sorting by query params for orders route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,64 +2,87 @@ const orderModel = require('../models/order');
 const moment = require('moment');
 
 async function order(req, res) {
-    const body = req.body
+    try {
+        const body = req.body
 
-    const total_price = body.items.reduce((prev, curr) => {
-        prev += curr.price
-        return prev
-    }, 0)
+        const total_price = body.items.reduce((prev, curr) => {
+            prev += curr.price
+            return prev
+        }, 0)
 
-    const order = await orderModel.create({
-        items: body.items,
-        created_at: moment().toDate(),
-        total_price
-    })
-    return res.json({ status: true, order })
+        const order = await orderModel.create({
+            items: body.items,
+            created_at: moment().toDate(),
+            total_price
+        })
+        return res.json({ status: true, order })
+    } catch (error) {
+        return res.json({ status: false, error })
+    }
+
 }
 
 async function getOrderById(req, res) {
-    const { orderId } = req.params;
+    try {
+        const { orderId } = req.params;
 
-    const order = await orderModel.findById(orderId)
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
+        const order = await orderModel.findById(orderId)
+        if (!order) {
+            return res.status(404).json({ status: false, order: null })
+        }
+
+        return res.json({ status: true, order })
+    } catch (error) {
+        return res.json({ status: false, error })
     }
-
-    return res.json({ status: true, order })
 }
 
 async function getAllOrder(req, res) {
-    const orders = await orderModel.find()
+    try {
+        let query = req.query
 
-    return res.json({ status: true, orders })
+        const orders = await orderModel.find({}).sort(query)
+
+        return res.json({ status: true, orders })
+    } catch (error) {
+        return res.status(400).json({ status: false, message: "Invalid query parameter" })
+    }
 }
 
 async function updateOrder(req, res) {
-    const { id } = req.params;
-    const { state } = req.body
+    try {
+        const { id } = req.params;
+        const { state } = req.body
 
-    const order = await orderModel.findById(id)
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
+        const order = await orderModel.findById(id)
+        if (!order) {
+            return res.status(404).json({ status: false, order: null })
+        }
+
+        if (state < order.state) {
+            return res.status(422).json({ status: false, order: null, message: "Invalid Operation" })
+        }
+
+        order.state = state
+
+        await order.save()
+
+        return res.json({ status: true, order })
+    } catch (error) {
+        return res.json({ status: false, error })
     }
-
-    if (state < order.state) {
-        return res.status(422).json({ status: false, order: null, message: "Invalid Operation" })
-    }
-
-    order.state = state
-
-    await order.save()
-
-    return res.json({ status: true, order })
 }
 
 async function deleteOrder(req, res) {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const order = await orderModel.deleteOne({ _id: id })
+        const order = await orderModel.deleteOne({ _id: id })
 
-    return res.json({ status: true, order })
+        return res.json({ status: true, order })
+    } catch (error) {
+        return res.json({ status: false, error })
+    }
 }
 
 module.exports = {
