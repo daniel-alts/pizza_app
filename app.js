1a279c5b1f3c33ad1cabd16c6a17aaac3c707e0e
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-09-29 14:13:49 +0100

    jest setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const session = require("express-session");
+const userRoute = require("./routes/user.routes");
+const orderRoute = require("./routes/order.routes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+	session({
+		secret: "secret",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+app.use("/user", userRoute);
+
+app.use("/order", orderRoute);
+
+app.get("/", (req, res) => {
+	return res.json({ status: true });
+});
+
+module.exports = app;
