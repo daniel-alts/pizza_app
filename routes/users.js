0d2b88f9ce61ff8729877fdc3a0775d927f046fe
Tobi Balogun <tobi.balo@gmail.com>
2022-09-30 23:09:01 +0100

    fix: extract users controllers from routes/users as standalone module

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const userModel = require('../models/userModel')
-const bcrypt = require('bcrypt')
+const controller = require('../controllers/usersController')
+const authenticate = require('../middleware/authenticate')
 
 /**
  * Get all users
  */
-router.route('/').get(async (req, res, next) => {
-  try {
-    const users = await userModel.find({}, { username: 1, user_type: 1 })
-
-    return res.json({ status: true, users })
-  } catch (err) {
-    return res.status(500).json({ status: false, data: err.message })
-  }
-})
+router.route('/').get(authenticate, controller.getAllUsers)
 
 /**
  * Create a new user
  */
-router.route('/register').post(async (req, res) => {
-  try {
-    const { username, password, user_type } = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const userObject = {
-      username,
-      password: hashedPassword,
-    }
-    if (user_type) userObject.user_type = user_type
-    const user = new userModel(userObject)
-    user
-      .save()
-      .then((result) => {
-        const { id, username, user_type } = result
-        const returnObj = {
-          id,
-          username,
-          user_type,
-        }
-        return res.status(201).json({ status: true, data: returnObj })
-      })
-      .catch((err) => {
-        console.log('error occured', err)
-        return res.status(400).json({ status: false, message: err.message })
-      })
-  } catch (err) {
-    res.json(err)
-  }
-})
+router.route('/register').post(controller.createUser)
 
 module.exports = router
