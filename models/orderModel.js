21d6550ed0fc301a07d72b9f36dae0e49d1569d8
Tobi Balogun <tobi.balo@gmail.com>
2022-10-01 05:33:46 +0100

    fix: update order JSON response from database

--- models/orderModel.js
+++ models/orderModel.js
@@ -19,13 +19,19 @@ const OrderSchema = new Schema({
 /**
  * Convert id to string
  * Remove id object from response
- * Remove _v from response
+ * Remove __v from response
+ * Convert item id to item_id as string
+ * Remove id object from item
  */
 OrderSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
+    returnedObject.items.forEach(item => {
+      item.item_id = item._id.toString()
+      delete item._id
+    })
   },
 })
 
