ea2a5484b4103352f2716da350979f0deee48a22
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-01 21:51:48 +0100

    order router test

--- src/resources/order/__tests__/order.router.test.js
+++ src/resources/order/__tests__/order.router.test.js
@@ -1,18 +1,25 @@
-const controllers = require('../order.controller')
-const { isFunction } = require('lodash')
+const router = require('../order.router')
 
-describe('order controllers', () => {
-    it('checks whether there is a crud controller', () => {
-        const crudMethods = [
-            'createOrder',
-            'checkOrderById',
-            'checkAllOrder',
-            'orderState',
-            'deleteOrder'
-        ]
+describe('order router', () => {
+	it('has crud routes', () => {
+		const routes = [
+			{ path: '/', method: 'get' },
+			{ path: '/', method: 'post' },
+			{ path: '/:id', method: 'get' },
+			{ path: '/:id', method: 'patch' },
+			{ path: '/:id', method: 'delete' },
+		]
 
-        crudMethods.forEach(name =>{
-            expect(isFunction(controllers[name])).toBe(true)
-        })
-    })
+		//loops through the routes above
+		routes.forEach((route) => {
+            console.log(router.stack)
+			// .stack stacks up the route in layers with respect to the route
+			const match = router.stack.find(
+				(s) =>
+					s.route.path === route.path &&
+					s.route.methods[route.method],
+			)
+			expect(match).toBeTruthy()
+		})
+	})
 })
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,87 +1,107 @@
-const moment = require('moment');
+const moment = require('moment')
 
+const createOrder =
+	(model) => async (req, res) => {
+		const body = req.body
 
-const createOrder = model => async (req, res) => {
-    const body = req.body;
+		try {
+			const order = await model.create({
+				items: body.items,
+			})
+			return res
+				.status(201)
+				.json({ data: order })
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
-    try {
-        const order = await model.create({ 
-            items: body.items,
-        })
-        return res.status(201).json({ data: order })
-    } catch (err) {
-        console.log(err)
-    }
-}
+const checkOrderById =
+	(model) => async (req, res) => {
+		const { id } = req.params
+		const order = await model.findById(id)
 
+		if (!order) {
+			return res.status(404).json({
+				status: false,
+				order: null,
+			})
+		}
 
-const checkOrderById = model => async (req,res) => {
-    const { id } = req.params;
-    const order = await model.findById(id)
+		return res
+			.status(200)
+			.json({ data: order })
+	}
 
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
+const checkAllOrder =
+	(model) => async (req, res) => {
+		try {
+			const orders = await model.find()
+			return res
+				.status(200)
+				.json({ data: orders })
+		} catch {
+			return res.status(404)
+		}
+	}
 
-    return res.status(200).json({ data: order })
-}
+const orderState =
+	(model) => async (req, res) => {
+		const { id } = req.params
+		const { state } = req.body
 
+		const order = await model.findById(id)
 
-const checkAllOrder = model => async (req, res) => {
-    try{
-        const orders = await model.find()
-        return res.status(200).json({ data: orders })
-    } catch {
-        return res.status(404)
-    }
-}
+		if (!order) {
+			return res.status(404).json({
+				status: false,
+				order: null,
+			})
+		}
 
+		if (state < order.state) {
+			return res.status(422).json({
+				status: false,
+				order: null,
+				message: 'Invalid operation',
+			})
+		}
 
-const orderState = model => async (req, res) => {
-    const { id } = req.params;
-    const { state } = req.body;
+		order.state = state
 
-    const order = await model.findById(id)
+		await order.save()
 
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
+		return res
+			.status(200)
+			.json({ data: order })
+	}
 
-    if (state < order.state) {
-        return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
-    }
+const deleteOrder =
+	(model) => async (req, res) => {
+		const { id } = req.params
 
-    order.state = state;
+		const order = await model.deleteOne({
+			_id: id,
+		})
 
-    await order.save()
+		return res
+			.status(200)
+			.json({ data: order })
+	}
 
-    return res.status(200).json({ data: order })
-}
-
-
-const deleteOrder = model => async (req, res) => {
-    const { id } = req.params;
-
-    const order = await model.deleteOne({ _id: id})
-
-    return res.status(200).json({ data: order })
-}
-
-
-const crudControllers = model => ({
-    createOrder: createOrder(model),
-    checkOrderById: checkOrderById(model),
-    checkAllOrder: checkAllOrder(model),
-    orderState: orderState(model),
-    deleteOrder: deleteOrder(model)
+const crudControllers = (model) => ({
+	createOrder: createOrder(model),
+	checkOrderById: checkOrderById(model),
+	checkAllOrder: checkAllOrder(model),
+	orderState: orderState(model),
+	deleteOrder: deleteOrder(model),
 })
 
 module.exports = {
-    createOrder,
-    checkOrderById,
-    checkAllOrder,
-    orderState,
-    deleteOrder,
-    crudControllers
+	createOrder,
+	checkOrderById,
+	checkAllOrder,
+	orderState,
+	deleteOrder,
+	crudControllers,
 }
-
