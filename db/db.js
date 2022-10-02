e86704d041f556639e39c6979f0268c7d85d9ec6
Eleos <Peaceaanu247@gmail.com>
2022-10-02 23:58:28 +0100

    Set test environment db variable

--- db/db.js
+++ db/db.js
@@ -2,18 +2,35 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 
 const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL
+const MONGODB_TEST_CONNECTION_URL = process.env.MONGODB_TEST_CONNECTION_URL
 
-function connectToMongoDb() {
-    mongoose.connect(MONGODB_CONNECTION_URL)
+if (process.env.NODE_ENV == 'test') {
+    function connectToMongoDb() {
+        mongoose.connect(MONGODB_TEST_CONNECTION_URL)
 
-    mongoose.connection.on("connected", () => {
-        console.log("Connected to MongoDB Successfully");
-    })
+        mongoose.connection.on("connected", () => {
+            console.log("Connected to MongoDB Successfully");
+        })
 
-    mongoose.connection.on("error", (err) => {
-        console.log("An error occurred while connecting to MongoDB");
-        console.log(err);
-    })
+        mongoose.connection.on("error", (err) => {
+            console.log("An error occurred while connecting to MongoDB");
+            console.log(err);
+        })
+    }
+} else {
+    function connectToMongoDb() {
+        mongoose.connect(MONGODB_CONNECTION_URL)
+
+        mongoose.connection.on("connected", () => {
+            console.log("Connected to MongoDB Successfully");
+        })
+
+        mongoose.connection.on("error", (err) => {
+            console.log("An error occurred while connecting to MongoDB");
+            console.log(err);
+        })
+    }
 }
+
 
 module.exports = { connectToMongoDb }
