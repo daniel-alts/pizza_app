33fd5e6a45fe320191080139481b951e2fc24573
Gbolahan <molubode81@gmail.com>
2022-10-02 16:42:07 +0100

    my commit

--- schemas/ordersModel.js
+++ schemas/ordersModel.js
@@ -1,20 +1,33 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate')
+
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const OrderSchema = new Schema({
   id: ObjectId,
+
   created_at: Date,
-  state: { type: Number, default: 1 },
+
+  state: {
+    type: Number,
+    default: 1
+  },
+
   total_price: Number,
+
   items: [{
     name: String,
     price: Number,
-    size: { type: String, enum: ['m', 's', 'l']},
+    size: { type: String, enum: ['m', 's', 'l'] },
     quantity: Number,
   }]
 });
+
+
+OrderSchema.plugin(mongoosePaginate)
+
 
 const Order = mongoose.model('Order', OrderSchema);
 
---
+++ test/ordersRoutes.test.js
@@ -0,0 +1,14 @@
+const app = require('../index')
+const supertest = require('supertest')
+const ordersRoutes = require('../routes/ordersRoutes')
+const usersRoutes = require('../routes/usersRoutes')
+
+
+describe('app', () => {
+    it('Should get all orders', async () => {
+        const response = await supertest(app).get('/orders?username=ssamuel&password=myguyy')
+        expect(response.headers["content-type"]).toBe("application/json")
+        expect(response.status).toBe(200)
+    })
+
+})
