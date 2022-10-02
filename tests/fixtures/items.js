37b386162be4354b44c770c8fa5191858538018e
odelolajosh <oladapoodelola@gmail.com>
2022-10-02 17:46:31 +0100

    test: add test data

---
+++ tests/fixtures/items.js
@@ -0,0 +1,27 @@
+module.exports = [
+  [
+    { price: 1.34, name: 'banana', size: 'm', quantity: 4 },
+    { price: 0.60, name: 'bread', size: 's', quantity: 2 },
+    { price: 3.98, name: 'broccoli', size: 'l', quantity: 2 },
+  ],
+  [
+    { price: 1.09, name: 'cherry', size: 's', quantity: 7 },
+    { price: 0.62, name: 'coconut', size: 's', quantity: 3 },
+    { price: 9.44, name: 'chocolate', size: 'l', quantity: 2 },
+  ],
+  [
+    { price: 0.99, name: 'egg', size: 's', quantity: 6 },
+  ],
+  [
+    { price: 0.99, name: 'egg', size: 's', quantity: 12 },
+    { price: 0.62, name: 'coconut', size: 's', quantity: 1 },
+  ],
+  [
+    { price: 0.60, name: 'bread', size: 's', quantity: 5 },
+    { price: 1.09, name: 'cherry', size: 's', quantity: 10 },
+  ],
+  [
+    { price: 1.23, name: 'cofee', size: 's', quantity: 2 },
+    { price: 1.09, name: 'cherry', size: 's', quantity: 10 },
+  ]
+]
