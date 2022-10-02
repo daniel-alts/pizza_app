34fb6001f6efe4585c9fe7fbca0fd9b8a1c06bd0
odelolajosh <oladapoodelola@gmail.com>
2022-10-02 17:47:20 +0100

    feat: make connection except in test mode

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const requireAuth = require('./middlewares/requireAuth');
 const orderRouter = require('./routes/order.route');
 const userRouter = require('./routes/user.route');
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 const PORT = 3334
 
@@ -10,27 +15,28 @@ const app = express()
 
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-	return res.json({ status: true })
+	return res.status(200).send('hello world')
 })
 
 app.use('/user', userRouter)
-// Protect all routes under /order
 app.use('/order', requireAuth, orderRouter)
 
+if (process.env.NODE_ENV !== 'test') {
+	mongoose.connect(MONGO_URI)
 
-mongoose.connect('mongodb://localhost:27017/pizza')
+	mongoose.connection.on("connected", () => {
+		console.log("Connected to MongoDB Successfully");
+	});
 
-mongoose.connection.on("connected", () => {
-	console.log("Connected to MongoDB Successfully");
-});
+	mongoose.connection.on("error", (err) => {
+		console.log("An error occurred while connecting to MongoDB");
+		console.error(err);
+	});
 
-mongoose.connection.on("error", (err) => {
-	console.log("An error occurred while connecting to MongoDB");
-	console.log(err);
-});
+	app.listen(PORT, () => {
+		console.log('Listening on port, ', PORT)
+	})
+}
 
-app.listen(PORT, () => {
-	console.log('Listening on port, ', PORT)
-})
+module.exports = app
