6329c6f43dd50d4b410e137dd116ee504ed8f865
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-03 17:59:39 +0100

    TOKEN system added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,33 +3,72 @@ const router = require('express').Router();
 const AppError = require('../utils/AppError');
 const ResponseHandler = require('../utils/responseHandler');
 const Catch_Async = require('../utils/catchAsync');
-const catchAsync = require('../utils/catchAsync');
-
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const { findById } = require('../Models/userModels');
 // global user variable
 // !be careful
 let user;
 
-// register
+const SIGNTOKEN = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
+
+// !register
 exports.Register = Catch_Async(async (req, res, next) => {
-	const { username, password, passwordConfirm } = req.body;
+	const { username, password, email, role } = req.body;
 
 	user = await Users.create({
 		username: username,
 		password: password,
+		email: email,
 	});
 
+	const TOKEN = SIGNTOKEN(user._id);
 	new ResponseHandler(res, user, 201);
 });
 
-// login
-exports.Login = catchAsync(async (req, res, next) => {
-	user = await Users.findOne({
-		username: req.body.username,
-	});
+// !login
+exports.Login = Catch_Async(async (req, res, next) => {
+	const { email, password } = req.body;
+	if (!email || !password)
+		return next(
+			new AppError('Please Provide the right credentials', 400),
+		);
+	user = await Users.findOne({ email }).select('password');
 
-	if (user.password !== req.body.password) {
-		return next(new AppError(' Wrong Credentials', 403));
+	if (!user || !(await user.checkPassword(password, user.password))) {
+		return next(new AppError('Login attempt failed', 400));
 	}
 
-	new ResponseHandler(res, user, 200);
+	const TOKEN = await SIGNTOKEN(user._id);
+	new ResponseHandler(res, user, 200, TOKEN);
 });
+
+exports.Auth = Catch_Async(async (req, res, next) => {
+	let TOKEN;
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		TOKEN = req.headers.authorization.split(' ')[1];
+	}
+
+	if (!TOKEN) {
+		return next(new AppError('Login to get accees', 401));
+	}
+
+	const decodeToken = await promisify(jwt.verify)(
+		TOKEN,
+		process.env.JWT_SECRET,
+	);
+
+	// Confirm user.
+	req.curUser = await Users.findById(decodeToken.id);
+	console.log(req.curUser);
+	next();
+});
+
+exports.Protect = Catch_Async(async (req, res, next) => {});
--- routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const auth = require('../controllers/auth');
-
-router.route('/login').post(auth.Login);
-
-module.exports = router;
