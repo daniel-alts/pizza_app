6376f5b935833caa6c94d4804891f491ebf3e052
Anthonia Nwanya <tonianwanya@gmail.com>
2022-10-04 23:36:29 +0100

    Added authentication, created user route and addedd query params to the order route

---
+++ database.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+function connectToMongoDb(){
+    mongoose.connect(process.env.MONGOCONNECT_URI)
+
+    mongoose.connection.on("connected", () => {
+        console.log("Connected to MongoDB Successfully");
+    });
+    
+    mongoose.connection.on("error", (err) => {
+        console.log("An error occurred while connecting to MongoDB");
+        console.log(err);
+    });
+}
+
+module.exports = { connectToMongoDb };
