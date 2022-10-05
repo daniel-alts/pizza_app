db8275da66bab21b7f084d7a9219bbc866a40e83
unknown <jjatuah@gmail.com>
2022-10-05 19:37:18 +0100

    completed authentication for the order routes

--- index.js
+++ index.js
@@ -25,67 +25,95 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/order', async (req, res) => {
-    const body = req.body.order;
+app.post('/order', (req, res) => {
+    authenticate (req,res)
+        .then(() => {
+            const body = req.body.order;
 
-    const total_price = body.items.reduce((prev, curr) => {
-        prev += curr.price
-        return prev
-    }, 0);
+            const total_price = body.items.reduce((prev, curr) => {
+                prev += curr.price
+                return prev
+            }, 0);
 
-    const order = await orderModel.create({ 
-        items: body.items,
-        created_at: moment().toDate(),
-        total_price
-    })
+            const order = orderModel.create({ 
+                items: body.items,
+                created_at: moment().toDate(),
+                total_price
+            })
+            
+            return res.json({ status: true, order })
+        }).catch((err) => {
+            return res.json({ status: false, message: err })
+        })
+})
+
+app.get('/order/:orderId', (req, res) => {
+    authenticate(req,res)
+        .then(() => {
+            const { orderId } = req.params;
+            const order =  orderModel.findById(orderId)
+
+            if (!order) {
+                return res.status(404).json({ status: false, order: null })
+            }
+
+            return res.json({ status: true, order })
+        })
+        }).catch((err) => {
+            return res.json({ status: false, message: err })
+        })
     
-    return res.json({ status: true, order })
+
+app.get('/orders', (req, res) => {
+    authenticate(req,res)
+        .then(() => {
+            const orders = orderModel.find()
+
+            return res.json({ status: true, orders })
+        }).catch((err) => {
+            return res.json({ status: false, message: err })
+        })
 })
 
-app.get('/order/:orderId', async (req, res) => {
-    const { orderId } = req.params;
-    const order = await orderModel.findById(orderId)
+app.patch('/order/:id', (req, res) => {
+    authenticate(req,res)
+        .then(() => {
+            const { id } = req.params;
+            const { state } = req.body.order;
 
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
+            const order = orderModel.findById(id)
 
-    return res.json({ status: true, order })
+            if (!order) {
+                return res.status(404).json({ status: false, order: null })
+            }
+
+            if (state < order.state) {
+                return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
+            }
+
+            order.state = state;
+
+            order.save()
+
+            return res.json({ status: true, order })
+        }).catch((err) => {
+            return res.json({ status: false, message: err })
+        })
+
 })
 
-app.get('/orders', async (req, res) => {
-    const orders = await orderModel.find()
+app.delete('/order/:id', (req, res) => {
+    authenticate(req,res)
+        .then(() => {
+            const { id } = req.params;
 
-    return res.json({ status: true, orders })
-})
+            const order = orderModel.deleteOne({ _id: id})
 
-app.patch('/order/:id', async (req, res) => {
-    const { id } = req.params;
-    const { state } = req.body.order;
+            return res.json({ status: true, order })
+        }).catch((err) => {
+            return res.json({ status: false, message: err })
+        })
 
-    const order = await orderModel.findById(id)
-
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
-
-    if (state < order.state) {
-        return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
-    }
-
-    order.state = state;
-
-    await order.save()
-
-    return res.json({ status: true, order })
-})
-
-app.delete('/order/:id', async (req, res) => {
-    const { id } = req.params;
-
-    const order = await orderModel.deleteOne({ _id: id})
-
-    return res.json({ status: true, order })
 })
 
 
