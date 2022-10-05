432f65b9fe985fa07bc050e91ba7fe3839e9c7a5
Adediran Kehinde <kennydemola2@gmail.com>
2022-10-05 05:43:41 +0100

    app modified and working

---
+++ basicAuthentication.js
@@ -0,0 +1,32 @@
+const dbModel= require('./models/userModel')
+
+
+
+
+module.exports = async (req, res, next) => {
+    // get basic authentication from header
+    const authorization = req.headers.authorization
+    try {
+      if (authorization) {
+        // remove "Basic "
+        const encoded = authorization.substring(6)
+        // decode to get username and password as plain text
+        const decoded = Buffer.from(encoded, 'base64').toString('ascii')
+        const [username, password] = decoded.split(':')
+        // get the user object from database
+        const authenticatedUser = await dbModel.findOne({ username })
+        // compare the password
+        // const match = await bcrypt.compare(password, authenticatedUser.password)
+        // if successful, store the details in the request
+        if (authenticatedUser) {
+          req.authenticatedUser = {
+            username: authenticatedUser.username,
+            role: authenticatedUser.user_type,
+          }
+        }
+      }
+      next()
+    } catch (err) {
+      next()
+    }
+  }
