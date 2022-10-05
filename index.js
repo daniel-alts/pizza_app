60b79fde69faa3b21dbb0bf52915af09607cac83
Blakcoder <anslemkay180@gmail.com>
2022-10-05 19:11:05 +0100

    All Tasks Executed

--- index.js
+++ index.js
@@ -1,95 +1,213 @@
-const express = require('express');
-const moment = require('moment');
-const mongoose = require('mongoose');
-const orderModel = require('./orderModel');
+const express = require('express')
+const moment = require('moment')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const orderModel = require('./orderModel')
+const User = require('./userModel')
+const dotenv = require('dotenv')
 
-const PORT = 3334
+dotenv.config({ path: './config.env' })
+
+const PORT = process.env.PORT
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json())
+app.use(cookieParser())
 
+// User Authentication
+app.post('/signup', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+      user_type: req.body.user_type,
+    })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        newUser,
+      },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: `User encountered ${err}`,
+    })
+  }
+})
+
+app.post('/login', async (req, res, next) => {
+  try {
+    const token = 'Authenticated'
+    const cookieOptions = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    }
+    //1. Grab username from request body.
+    const { username, password } = req.body
+    //2. check if username exists in database
+    const currentUser = await User.findOne({ username })
+    //3. encrypt provided password and check if it exists
+    if (
+      !currentUser ||
+      !(await currentUser.correctPassword(password, currentUser.password))
+    ) {
+      next(err)
+    }
+    res.cookie('token', token, cookieOptions)
+    res.status(201).json({
+      status: 'Success',
+      message: 'Login Successful',
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Incorrect Password or Username`,
+    })
+  }
+})
 
 app.get('/', (req, res) => {
-    return res.json({ status: true })
+  return res.json({ status: true })
 })
 
-
-app.post('/order', async (req, res) => {
-    const body = req.body;
-
-    const total_price = body.items.reduce((prev, curr) => {
-        prev += curr.price
-        return prev
-    }, 0);
-
-    const order = await orderModel.create({ 
-        items: body.items,
-        created_at: moment().toDate(),
-        total_price
+const protectRoute = async (req, res, next) => {
+  const token = req.cookies.token
+  try {
+    if (token == 'Authenticated') {
+      return next()
+    }
+    return next(err)
+  } catch (err) {
+    res.status(403).json({
+      status: 'Failed',
+      message: `Please Login`,
     })
-    
-    return res.json({ status: true, order })
+  }
+}
+app.post('/order', protectRoute, async (req, res) => {
+  console.log(req.cookies.token)
+  const body = req.body
+
+  const total_price = body.items.reduce((prev, curr) => {
+    prev += curr.price
+    return prev
+  }, 0)
+
+  const order = await orderModel.create({
+    items: body.items,
+    created_at: moment().toDate(),
+    total_price,
+    state: req.body.state,
+  })
+
+  return res.json({ status: true, order })
 })
+
+// app.get('/order/:sort', async (req, res, next) => {
+//   const queryObj = { ...req.query }
+//   const excludedFields = ['page', 'sort', 'limit', 'fields']
+//   excludedFields.forEach((el) => delete queryObj[el])
+
+//   const { sort } = req.params
+
+//   let query = orderModel.find(queryObj)
+//   if (sort) {
+//     query = query.sort(sort)
+//   }
+//   const order = await query
+
+//   res.status(200).json({
+//     status: 'Success',
+//     result: order.length,
+//     data: {
+//       order,
+//     },
+//   })
+// })
 
 app.get('/order/:orderId', async (req, res) => {
-    const { orderId } = req.params;
-    const order = await orderModel.findById(orderId)
+  const { orderId } = req.params
+  const order = await orderModel.findById(orderId)
+  console.log(req.params)
+  if (!order) {
+    return res.status(404).json({ status: false, order: null })
+  }
 
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
-
-    return res.json({ status: true, order })
+  return res.json({ status: true, order })
 })
 
-app.get('/orders', async (req, res) => {
-    const orders = await orderModel.find()
+app.get('/orders/:sort?', async (req, res) => {
+  const queryObj = { ...req.query }
+  const excludedFields = ['page', 'sort', 'limit', 'fields']
+  excludedFields.forEach((el) => delete queryObj[el])
 
-    return res.json({ status: true, orders })
+  const { sort } = req.params
+
+  let query = orderModel.find(queryObj)
+  if (sort) {
+    query = query.sort(sort)
+  }
+  const page = req.query.page * 1 || 1
+  const limit = req.query.limit * 1 || 5
+  const skip = (page - 1) * limit
+
+  query = query.skip(skip).limit(limit)
+  const orders = await query
+
+  return res.json({
+    status: true,
+    results: orders.length,
+    orders,
+  })
 })
 
 app.patch('/order/:id', async (req, res) => {
-    const { id } = req.params;
-    const { state } = req.body;
+  const { id } = req.params
+  const { state } = req.body
 
-    const order = await orderModel.findById(id)
+  const order = await orderModel.findById(id)
 
-    if (!order) {
-        return res.status(404).json({ status: false, order: null })
-    }
+  if (!order) {
+    return res.status(404).json({ status: false, order: null })
+  }
 
-    if (state < order.state) {
-        return res.status(422).json({ status: false, order: null, message: 'Invalid operation' })
-    }
+  if (state < order.state) {
+    return res
+      .status(422)
+      .json({ status: false, order: null, message: 'Invalid operation' })
+  }
 
-    order.state = state;
+  order.state = state
 
-    await order.save()
+  await order.save()
 
-    return res.json({ status: true, order })
+  return res.json({ status: true, order })
 })
 
 app.delete('/order/:id', async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params
 
-    const order = await orderModel.deleteOne({ _id: id})
+  const order = await orderModel.deleteOne({ _id: id })
 
-    return res.json({ status: true, order })
+  return res.json({ status: true, order })
 })
 
+mongoose.connect(process.env.DATABASE_LOCAL)
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB Successfully')
+})
 
-mongoose.connection.on("connected", () => {
-	console.log("Connected to MongoDB Successfully");
-});
-
-mongoose.connection.on("error", (err) => {
-	console.log("An error occurred while connecting to MongoDB");
-	console.log(err);
-});
+mongoose.connection.on('error', (err) => {
+  console.log('An error occurred while connecting to MongoDB')
+  console.log(err)
+})
 
 app.listen(PORT, () => {
-    console.log('Listening on port, ', PORT)
+  console.log('Listening on port, ', PORT)
 })
+
+console.log(process.env.NODE_ENV)
