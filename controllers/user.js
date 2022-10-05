849a90e23663cf6e243fd4933f0722988ab2dc8a
Ann-tech <Onyekaann17@gmail.com>
2022-10-05 16:49:33 +0100

    added integration test for user route

--- controllers/user.js
+++ controllers/user.js
@@ -9,8 +9,7 @@ async function signup(req, res) {
         if (countUsers == 0) userDetail._id = 1
         else userDetail._id = countUsers + 1
         const createUser = await userModel.create(userDetail)
-        res.json( {status: true, message: "Signup successful, Login to make your orders"})
-        return
+        return res.json( {status: true, message: "Signup successful, Login to make your orders"})
     }
     res.json( {status: false, message: "user already exists"})
 }
--- test/user.test.js
+++ test/user.test.js
@@ -1,32 +1,37 @@
-// let server = require('../index')
-// let supertest = require('supertest')
-// let users = require('../fixtures/user.json')
+let server = require('../index')
+let supertest = require('supertest')
+let users = require('../fixtures/user.json')
 
-// describe('user route', () => {
-//     // ADDS 1st user
-//     test('POST /user/signup - add first user', async () => {
-//         let res = await supertest(server).post("/user/signup").send(users[0])
-//         expect(response.status).toBe(201)
-//         expect(response.body.status).toBe("true")
-//     });
 
-//     test('POST /user/signup - add second user', async () => {
-//         let res = await supertest(server).post("/user/signup").send(users[1])
-//         expect(response.status).toBe(201)
-//         expect(response.body.status).toBe("true")
-//     });
+describe('user route', () => {
+    test('POST /user/signup - add first user', async () => {
+        let res = await supertest(server).post("/user/signup").send(users[0])
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({status: true, message: "Signup successful, Login to make your orders"})
+    }, 70000);
 
-//     test('POST /user/signup - add 3rd user', async () => {
-//         let res = await supertest(server).post("/user/signup").send(users[2])
-//         expect(response.status).toBe(201)
-//         expect(response.body.status).toBe("true")
-//     });
+    test('POST /user/signup - add second user', async () => {
+        let res = await supertest(server).post("/user/signup").send(users[1])
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({status: true, message: "Signup successful, Login to make your orders"})
+    }, 70000);
 
-//     test('POST /user/signup - add 4th user', async () => {
-//         let res = await supertest(server).post("/user/signup").send(users[3])
-//         console.log(res)
-//         expect(response.status).toBe(201)
-//         expect(response.body.status).toBe("true")
-//     });
+    test('POST /user/signup - add 3rd user', async () => {
+        let res = await supertest(server).post("/user/signup").send(users[2])
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({status: true, message: "Signup successful, Login to make your orders"})
+    }, 70000);
+
+    test('POST /user/signup - add 4th user', async () => {
+        let res = await supertest(server).post("/user/signup").send(users[3])
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({status: true, message: "Signup successful, Login to make your orders"})
+    }, 70000);
+
+    test('POST /user/login - login first user', async () => {
+        let res = await supertest(server).post("/user/login").set("Authentication", `Bearer ${users[0].username} ${users[0].password} ${users[0].email}`)
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({status: true, message: "Login successful, Visit the order routes to make your orders"})
+    }, 40000);
     
-// })
+})
