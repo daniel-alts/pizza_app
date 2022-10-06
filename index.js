06e337ff88c0c133d397f35b00c275d421e51556
teejay128 <104542152+Teejay128@users.noreply.github.com>
2022-10-06 23:23:35 +0100

    successfully added basic authentication using bcrypt

--- index.js
+++ index.js
@@ -2,18 +2,52 @@ const express = require('express');
 const moment = require('moment');
 const mongoose = require('mongoose');
 const orderModel = require('./models/orderModel');
+const userModel = require('./models/userModel');
+const bcrypt = require('bcrypt');
 
 const PORT = 3334
 
 const app = express();
 
-app.use(express.json());
+const users = []
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.json({ status: true });
 });
 
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
+    };
+});
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find((user) => user.name = req.body.name);
+
+    if(user == null){
+        return res.status(400).send('cannot find user');
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Success')
+        } else {
+            res.send('Access Denied');
+        };
+    } catch {
+        res.status(500).send();
+    };
+})
 
 app.post('/order', async (req, res) => {
     const body = req.body;
