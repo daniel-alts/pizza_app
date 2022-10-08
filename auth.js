e54020156e2b960bb842e7f35b5a98163efb8d48
B <henryirons@yahoo.com>
2022-10-08 20:25:51 +0100

    auth.js is fixed, can send token in body of request or as a header

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const config = process.env;
 
 const verifyToken = (req, res, next) => {
   const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+    req.body.token || req.query.token || req.headers["authorization"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
