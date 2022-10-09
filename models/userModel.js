f5030a9f6a31e4f2465b9f0414910d544eb2ee30
Violacordis <ikemviolacordis@gmail.com>
2022-10-09 11:20:30 +0100

    Installed a npm package called validator and required it on my user model where i used it to validate any user email input

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
+const validator = require("validator");
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, "Please provide a first name"],
-  },
-  lastName: {
-    type: String,
-    required: [true, "Please provide a last name"],
-  },
   username: {
     type: String,
-    required: true,
-    minLength: [4, "Your username should not be less than 4 characters"],
-    maxLength: [15, "Your username should not be more than 15 characters"],
+    required: [true, "Please tell us your name"],
     unique: [true, "This username is already taken"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   password: {
     type: String,
-    required: true,
-    minLength: [6, "Your password should not be less than 6 characters"],
-    maxLength: [15, "Your password should not be more than 15 characters"],
+    required: [true, "Please provide a password"],
+    minLength: [8, "Your password should not be less than 8 characters"],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
   },
   user_type: {
     type: String,
@@ -35,6 +36,6 @@ const userSchema = new Schema({
   },
 });
 
-const user = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = user;
+module.exports = User;
