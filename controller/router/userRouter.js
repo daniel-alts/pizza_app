2675837a5f37154e270320036970dab1fe460b53
Emmanuel Eni <eejeba@gmail.com>
2022-10-09 21:59:17 +0100

    Integration test

---
+++ controller/router/userRouter.js
@@ -0,0 +1,3 @@
+const { Router } = require('express');
+
+const router = Router();
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { config } = require('dotenv');
-// const mongoose = require('mongoose');
-const moment = require('moment');
-const { connectMongoDB } = require('./database/connection');
-const orderModel = require('./model/orderModel');
+const { connectMongoDB } = require('./model/db_settings/connection');
+const { router: orderRouter } = require('./controller/router/orderRouter');
+
+const app = express();
+const PORT = process.env.PORT || 3334;
 
 // dotenv config
 config();
@@ -11,80 +12,9 @@ config();
 // MongoDB connection
 connectMongoDB();
 
-const app = express();
-const PORT = process.env.PORT || 3334;
-
+// Middlewares
 app.use(express.json());
-
-// Post
-app.post('/order', async (req, res) => {
-  const body = req.body;
-
-  const total_price = body.items.reduce((prev, curr) => {
-    prev += curr.price;
-    return prev;
-  }, 0);
-
-  const order = await orderModel.create({
-    items: body.items,
-    created_at: moment().toDate(),
-    total_price,
-  });
-
-  return res.json({ status: true, order });
-});
-
-// Get by id
-app.get('/order/:orderId', async (req, res) => {
-  const { orderId } = req.params;
-  const order = await orderModel.findById(orderId);
-
-  if (!order) {
-    return res.status(404).json({ status: false, order: null });
-  }
-
-  return res.json({ status: true, order });
-});
-
-// Get all
-app.get('/order', async (req, res) => {
-  const orders = await orderModel.find();
-
-  return res.json({ status: true, orders });
-});
-
-// Patch
-app.patch('/order/:id', async (req, res) => {
-  const { id } = req.params;
-  const { state } = req.body;
-
-  const order = await orderModel.findById(id);
-
-  if (!order) {
-    return res.status(404).json({ status: false, order: null });
-  }
-
-  if (state < order.state) {
-    return res
-      .status(422)
-      .json({ status: false, order: null, message: 'Invalid operation' });
-  }
-
-  order.state = state;
-
-  await order.save();
-
-  return res.json({ status: true, order });
-});
-
-// Delete
-app.delete('/order/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const order = await orderModel.deleteOne({ _id: id });
-
-  return res.json({ status: true, order });
-});
+app.use('/order', orderRouter);
 
 app.listen(PORT, () => {
   console.log(
