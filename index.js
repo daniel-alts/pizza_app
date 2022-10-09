7f4b98c73f27b74e2ab3c6ae3ce33510647a55d9
Toba <tobajamezmadamori@gmail.com>
2022-10-09 17:42:51 +0100

    modified application instance

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express');
-
+const passport = require('passport')
 
 const app = express()
+
+app.use(express.json())
 
 const userRouter = require('./Users/routes')
 const orderRouter = require('./Orders/routes')
@@ -12,6 +14,12 @@ const authMiddleware = require('./Middleware/authentication')
 app.use(express.json());
 
 app.use('/users', userRouter)
-app.use('/', authMiddleware, orderRouter)
+app.use('/', passport.authenticate('jwt', { session: false }), orderRouter)
+
+app.use(function(err, req, res, next ){
+    console.log(err)
+    res.status(err.status || 500)
+    res.json({ error: err.message })
+})
 
 module.exports = app
