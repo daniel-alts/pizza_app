1a34ab484e2aa8092999ec5dddbf4b6004b209be
Violacordis <ikemviolacordis@gmail.com>
2022-10-10 21:33:23 +0100

    implemented signup functionality using jwt.sign() with the following parameters: payload(user ID), secret key and expiresIn option (60days) and created the signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,27 @@
+const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
+const catchAsyncError = require("../utils/catchAsyncError");
 
-exports.signup = async (req, res, next) => {
-  try {
-    const user = await userModel.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+exports.signup = catchAsyncError(async (req, res, next) => {
+  const newUser = await userModel.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  // Creating a token with payload(data to be used- using id), secret-key, and option like expireIn as parameter
+  const token = jwt.sign({ id: newUser._id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
+  });
+
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      newUser,
+    },
+  });
+});
