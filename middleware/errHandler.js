8be5cfa74251ea2cde8ddd5ed3d4c953e2f61aeb
Tobi Balogun <tobi.balo@gmail.com>
2022-10-11 14:28:29 +0100

    func: update error handling middleware with support for JsonWebToken errors

--- middleware/errHandler.js
+++ middleware/errHandler.js
@@ -3,8 +3,26 @@
  */
 module.exports = (error, req, res, next) => {
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'Unable to lookup order with provided ID' })
+    return res.status(400).send({
+      error: 'Unable to lookup order with provided ID',
+    })
   }
 
-  return res.status(500).send({ error: 'Sorry, error occured' })
+  if (error.name === 'JsonWebTokenError') {
+    return res.status(400).json({
+      error: 'invalid token',
+    })
+  }
+
+  if (error.name === 'TokenExpiredError') {
+    return res.status(400).json({
+      error: 'token expired',
+    })
+  }
+
+  res.status(400).json({
+    error: error.message,
+  })
+
+  next()
 }
