1ea33818bdacf1405b062fea659c28b1a844fe79
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-12 14:49:01 +0100

    new modification to the project set up and adding new features

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require("express");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
 const { connectToMongoDB } = require("./database/db");
-
 const orderRoute = require("./routes/order");
+const authRoute = require("./routes/auth");
 
 require("dotenv").config();
+require("./middlewares/authentication/auth");
 
 const PORT = 3334;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/", authRoute);
+app.use("/order", passport.authenticate("jwt", { session: false }), orderRoute);
+
+// renders the home page
 app.get("/", (req, res) => {
-  return res.json({ status: true });
+  return res.send("Welcome to the PIZZA API");
 });
 
-// USER
-
-app.post("/signup", async (req, res) => {
-  const body = req.body;
-
-  const user = await userModel.create(body);
-
-  return res.json({ status: true, user });
+// Handle errors.
+app.use(function(err, req, res, next) {
+  console.log(err);
+  res.status(err.status || 500);
+  res.json({ error: err.message });
 });
 
 connectToMongoDB(
