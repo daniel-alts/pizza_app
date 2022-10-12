302444e2cabb5234e5c846c466d48fef9e0fdccc
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-10-12 12:33:02 +0100

    reformed authenticate.js

--- utilities/authenticate.js
+++ utilities/authenticate.js
@@ -1,25 +1,24 @@
 const orderModel = require('../models/orderModel');
 const userModel = require('../models/userModel');
 
-const authenticateUser = (req, res) =>
+const authenticateUser = (req, _) =>
   new Promise(async (resolve, reject) => {
     const { username, password } = req.body;
     if (!username || !password) reject('Invalid username or password.');
     const [user] = await userModel.find({ username: username });
     if (!user) reject('User Not Found');
-    if (password !== user.password) reject('Invalid password');
+    if (password !== user.password) reject('Invalid username or password');
     else resolve('Done');
   });
 
 const authenticateOrder = (req, res, type) =>
   new Promise(async (resolve, reject) => {
-    if (!req.body) reject('Invalid order');
     const { id } = req.params;
     const order = await orderModel.findById(id);
     if (!order) reject('Invalid ID');
     if (type !== 'user' && type !== 'admin')
       reject("can't perform this operation");
-    else resolve('Access Granted');
+    else resolve('Access granted');
   });
 
 module.exports = { authenticateUser, authenticateOrder };
