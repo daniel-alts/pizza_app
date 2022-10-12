cf520eec7323dd75b7ad95a8e1603f2983f4cfa0
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-10-12 14:49:12 +0100

    new modification to the project set up and adding new features

--- models/userModel.js
+++ models/userModel.js
@@ -4,11 +4,6 @@ const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
   email: {
     type: String,
     required: true,
