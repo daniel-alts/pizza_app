5167c4aeaddc4be4d671796be73fecd9752df1d1
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-10-14 22:52:13 +0100

    updated the user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const passport = require('passport')
 
 const userRouter = express.Router();
 
 
 // USER ROUTER WITH SIGNUP AND LOGIN ROUTES
-userRouter.route('/signup')
-    .post(userController.signUp)
-userRouter.route('/login')
-    .post(userController.login)
+userRouter.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  userController.signup
+);
+
+userRouter.route("/login").post(userController.login);
 
 
     
