b7bf01ff6f1df1cb2094cd1216f5442b1aff75ac
Tobi Balogun <tobi.balo@gmail.com>
2022-10-14 17:57:16 +0100

    fix: protect order routes using passport-jwt

--- routes/orders.js
+++ routes/orders.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/orderController')
-const getToken = require('../middleware/getToken')
-const getUser = require('../middleware/getUser')
+const passport = require('passport')
 const authorize = require('../middleware/authorize')
 const checkId = require('../middleware/checkId')
 
-router.use(getToken, getUser)
+
+router.use(passport.authenticate('jwt', { session: false }))
 /**
  * Get information about all orders
  */
