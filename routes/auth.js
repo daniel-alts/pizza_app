e76f341a5441453078365e3ed8df044f7d61998c
Kachi <astrokachi@gmail.com>
2022-10-14 03:21:30 +0100

    updated auth routes to use local strategy

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const authRoute = express.Router()
-const { login, register, getUsers } = require('../controllers/auth')
+const passport = require('passport')
 
-authRoute.route('/login').post(login)
-authRoute.route('/signup').post(register)
+authRoute.post("/login", passport.authenticate("login", {session: false}), async(req, res) => {
+    res.json({
+        message: "successfully logged in",
+        user: req.user.user,
+        token: req.user.token
+    })
+})
+
+authRoute.post("/signup", passport.authenticate("signup", {session: false}), async(req, res) => {
+    res.json({
+        message: "Sign up successful",
+        user: req.user.user,
+        token: req.user.token
+    })
+
+})
+
 module.exports = authRoute
