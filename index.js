940347b1673c6dd894bb8b9ff38aba42f35f0a57
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-10-14 12:43:22 +0000

    passport js and jwt authentication

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+
+if(process.env.NODE_ENV !== 'production'){
+	require("dotenv").config();
+}
+
 const express = require('express');
 const moment = require('moment');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+
+
 const {orderRouter} = require('./Router/orderRouter')
-const {userRouter} = require('./Router/userRouter')
+const {userRouter} = require('./Router/userRouter');
+const passport = require('./passport/passport_config')
 const app = express()
-require("dotenv").config();
+
 const PORT = 3334
 app.use(express.json());
 app.use("/user", userRouter)
 app.use("/order", orderRouter) 
+
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({mongoUrl: 'mongodb+srv://Arthur_2002:1234arthur@cluster0.tzk9r.mongodb.net/?retryWrites=true&w=majority'})
+	})
+)
+
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 
 mongoose.connect('mongodb+srv://Arthur_2002:1234arthur@cluster0.tzk9r.mongodb.net/?retryWrites=true&w=majority')
--- middlewear/middlewear.js
+++ middlewear/middlewear.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModels");
+const bcrypt = require('bcrypt');
 
 // AN AUTHENTICATION TO PROTECT THE ORDER ROUTES
 const authenticateUser = async (req, res, next) => {
@@ -11,20 +12,92 @@ const authenticateUser = async (req, res, next) => {
   const encoded = authorization.substring(6);
 
   const decoded = Buffer.from(encoded, "base64").toString("ascii");
-
+ 
   const [username, password] = decoded.split(":");
 
   const authenticatedUser = await userModel.findOne({ username });
-
-  if (!authenticatedUser) {
-    return res.status(404).send({ message: "User not found" });
+  const match = await bcrypt.compare(password, authenticatedUser.password)
+  if(match){
+    req.authenticatedUser = authenticatedUser;
   }
 
-  if (authenticatedUser.password !== password) {
-    return res.status(404).send({ message: "password is incorrect" });
-  }
 
   next();
 };
 
 module.exports = { authenticateUser };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // MIDDLEWARE FOR AUTHORIZATION (MAKING SURE THEY ARE LOGGED IN)
+// const authenticateUser = async (req, res, next) => {
+//   try {
+    
+//     // check if auth header exists
+//     if (req.headers.authorization) {
+//       // parse token from header
+//       const token = req.headers.authorization.split(" ")[1]; //split the header and get the token
+//       if (token) {
+//         const payload = await jwt.verify(token, process.env.SECRET);
+//         if (payload) {
+//           // store user data in request object
+//           req.user = payload;
+//           next();
+//         } else {
+//           res.status(400).json({ error: "token verification failed" });
+//         }
+//       } else {
+//         res.status(400).json({ error: "malformed auth header" });
+//       }
+//     } else {
+//       res.status(400).json({ error: "No authorization header" });
+//     }
+//     const authorization = req.headers.authorization;
+//     if(authorization) {
+//       const encoded = authorization.substring(6);
+//       const decoded = Buffer.from(encoded, 'base64').toString('ascii');
+//       const [email, password] = decoded.split(':');
+//       const authenticatedUser = await userModel.findByEmail(email);
+//       const match = await bcrypt.compare(password, authenticatedUser.password);
+//       if(match){
+//         req.authenticatedUser = authenticatedUser;
+//       }
+//     }
+  
+//   } catch (error) {
+//     res.status(400).json({ error });
+//   }
+// };
+
+// // export custom middleware
+// module.exports = {
+//   authenticateUser,
+// };
+
