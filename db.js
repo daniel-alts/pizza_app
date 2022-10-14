4739646b8cb0a2ec4c40a4ded0e8dc0af796c3bc
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2022-10-14 23:54:24 +0100

    create a MongoDB instance

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose= require("mongoose")
+require("dotenv").config()
+
+const MONGO_DB_CONNECTION_URL= process.env.MONGO_DB_CONNECTION_URL
+
+function connectToMongoDB() {
+    mongoose.connect(MONGO_DB_CONNECTION_URL);
+
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB successfully');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('Error connecting to MongoDB', err);
+    })
+}
+
+module.exports = { connectToMongoDB };
