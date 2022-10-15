23fdbdee956cc8b480ee592694b918628870fab5
prosper <anosikeprosper2@gmail.com>
2022-10-15 06:51:07 -0700

    added authorization middleware to the order routes

--- index.js
+++ index.js
@@ -6,23 +6,45 @@ require("dotenv").config();
 
 const { connectToMongoDB } = require("./db/db");
 
+const passport = require("passport");
+const bodyParser = require("body-parser");
+
 const { orderRouter } = require("./router/orderRoute");
 const { userRouter } = require("./router/userRoute");
 
 const PORT = process.env.PORT;
 
+// const authRoute = require("./router/authRoute");
+
+require("./authentication/auth"); // Signup and login authentication middleware
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/order", orderRouter);
-app.use("/users", userRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/user", userRouter);
+
+app.use(
+  "/order",
+  passport.authenticate("jwt", { session: false }),
+  orderRouter
+);
+
+// app.use("/users", userRouter);
 
 connectToMongoDB();
+
+// Handle errors.
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port, ", PORT);
 });
 
-
-module.exports = app
+module.exports = app;
