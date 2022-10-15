ba3a74b4896c21417864c0db6dd6465739b4e73a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-16 00:18:34 +0100

    feat: add middleware to filter out non-admin users

--- middlewares.js
+++ middlewares.js
@@ -4,14 +4,14 @@ async function basicAuthMiddleware(req, res, next) {
     if (req.path == "/users/authenticate" || req.path == "/users/new") {
       return next();
     }
-  
+
     if (
       !req.headers.authorization ||
       req.headers.authorization.indexOf("Basic") === -1
     ) {
       return res.status(401).json({ message: "Missing Authorization Header" });
     }
-  
+
     const base64Credentials = req.headers.authorization.split(" ")[1];
     const credentials = Buffer.from(base64Credentials, "base64").toString("utf8");
     const [username, password] = credentials.split(":");
@@ -27,7 +27,7 @@ async function basicAuthMiddleware(req, res, next) {
       2: ["^/order/.*", "PATCH"],
       3: ["^/order/.*", "DELETE"]
     }
-    
+
     let permitted = true;
     Object.entries(adminPermissions).forEach(route => {
         if (req.path.match(route[1][0]) && route[1][1] === req.method && user.user_type !== "admin") {
@@ -44,4 +44,14 @@ async function basicAuthMiddleware(req, res, next) {
     }
 }
 
-module.exports = basicAuthMiddleware;
+function adminOnlyMiddleware(req, res, next) {
+  // res.setHeader('Content-Type', 'application/json');
+  if (req.user.userPermission === "admin") {
+    next();
+  } else {
+    res.statusCode = 401;
+    res.json({message: "You do no have this permission."})
+  }
+}
+
+module.exports = {basicAuthMiddleware, adminOnlyMiddleware};
