18130e3d163bd89b128becb9392527a9c8a85cec
neche <chi2064real@gmail.com>
2022-10-15 19:22:37 +0100

    piza assignment

---
+++ db.js
@@ -0,0 +1,18 @@
+
+
+// const mongoose = require('mongoose');
+// require("dotenv").config();
+
+// const MONGO_DB_CONNECTION_URL  = process.env.MONGO_DB_CONNECTION_URL;
+// function connectToMongoDB(){
+//     mongoose.connect(MONGO_DB_CONNECTION_URL )
+
+//     mongoose.connection.on("connected", ()=>{
+//         console.log("connected to MongoDB succesfully")
+//     })
+//     mongoose.connection.on("error", (err)=>{
+//         console.log(err)
+//         console.log("Error connected to DB")
+//     })
+// }
+// module.exports ={connectToMongoDB}
