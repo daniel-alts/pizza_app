2e0407363c6c56d8a11a1d1e9cba769b33f08792
Blakcoder <anslemkay180@gmail.com>
2022-10-15 21:33:43 +0100

    Added JWT Authentication

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const cookieParser = require('cookie-parser')
 const orderModel = require('./orderModel')
 const User = require('./userModel')
 const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+const JWTstrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
 
 dotenv.config({ path: './config.env' })
 
@@ -15,80 +21,122 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-// User Authentication
-app.post('/signup', async (req, res) => {
-  try {
-    const newUser = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-      user_type: req.body.user_type,
-    })
-    res.status(200).json({
-      status: 'success',
-      data: {
-        newUser,
-      },
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `User encountered ${err}`,
-    })
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_SECRET_EXPIRES,
+  })
+}
+
+const createSendToken = (user, res) => {
+  const token = signToken(user.id)
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
   }
-})
+  res.cookie('token', token, cookieOptions)
+}
+
+//Signup Using Passport - localStrategy
+
+passport.use(
+  'signup',
+  new localStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+      passReqToCallback: true,
+    },
+    async (req, username, password, done) => {
+      try {
+        const { user_type } = req.body.user_type
+        const user = await User.create({ username, password, user_type })
+        return done(null, user)
+      } catch (error) {
+        done(error)
+      }
+    },
+  ),
+)
+
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username })
+        if (!user || !(await user.correctPassword(password, user.password))) {
+          return done(null, false, {
+            message: 'Username or Password Incorrect',
+          })
+        }
+        return done(null, user, { message: 'Login Successful' })
+      } catch (err) {
+        done(err)
+      }
+    },
+  ),
+)
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token'),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user)
+      } catch (err) {
+        return done(err)
+      }
+    },
+  ),
+)
+
+// User Authentication //
+
+app.post(
+  '/signup',
+  passport.authenticate('signup', { session: false }),
+  async (req, res, next) => {
+    res.json({
+      status: 'success',
+      message: 'Login Successful',
+      user: req.user,
+    })
+  },
+)
 
 app.post('/login', async (req, res, next) => {
-  try {
-    const token = 'Authenticated'
-    const cookieOptions = {
-      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An error occurred')
+        return next(error)
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error)
+        const body = { _id: user._id }
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET)
+        res.json({ token })
+      })
+    } catch (err) {
+      return next(err)
     }
-    //1. Grab username from request body.
-    const { username, password } = req.body
-    //2. check if username exists in database
-    const currentUser = await User.findOne({ username })
-    //3. encrypt provided password and check if it exists
-    if (
-      !currentUser ||
-      !(await currentUser.correctPassword(password, currentUser.password))
-    ) {
-      next(err)
-    }
-    res.cookie('token', token, cookieOptions)
-    res.status(201).json({
-      status: 'Success',
-      message: 'Login Successful',
-    })
-  } catch (err) {
-    res.status(400).json({
-      status: 'failed',
-      message: `Incorrect Password or Username`,
-    })
-  }
+  })(req, res, next)
 })
 
 app.get('/', (req, res) => {
   return res.json({ status: true })
 })
 
-const protectRoute = async (req, res, next) => {
-  const token = req.cookies.token
-  try {
-    if (token == 'Authenticated') {
-      return next()
-    }
-    return next(err)
-  } catch (err) {
-    res.status(403).json({
-      status: 'Failed',
-      message: `Please Login`,
-    })
-  }
-}
-app.post('/order', protectRoute, async (req, res) => {
-  console.log(req.cookies.token)
+app.post('/order', async (req, res) => {
   const body = req.body
 
   const total_price = body.items.reduce((prev, curr) => {
@@ -103,30 +151,9 @@ app.post('/order', protectRoute, async (req, res) => {
     state: req.body.state,
   })
 
-  return res.json({ status: true, order })
+  return res.json({ status: true, order, token: req.query.secret_token })
 })
 
-// app.get('/order/:sort', async (req, res, next) => {
-//   const queryObj = { ...req.query }
-//   const excludedFields = ['page', 'sort', 'limit', 'fields']
-//   excludedFields.forEach((el) => delete queryObj[el])
-
-//   const { sort } = req.params
-
-//   let query = orderModel.find(queryObj)
-//   if (sort) {
-//     query = query.sort(sort)
-//   }
-//   const order = await query
-
-//   res.status(200).json({
-//     status: 'Success',
-//     result: order.length,
-//     data: {
-//       order,
-//     },
-//   })
-// })
 
 app.get('/order/:orderId', async (req, res) => {
   const { orderId } = req.params
--- userModel.js
+++ userModel.js
@@ -14,16 +14,16 @@ const userSchema = new Schema({
     required: [true, 'Please Provide Password'],
     minLength: 8,
   },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please Provide Password Confirm'],
-    validate: {
-      validator: function (el) {
-        return el === this.password
-      },
-      message: 'Password Do Not Match',
-    },
-  },
+  // passwordConfirm: {
+  //   type: String,
+  //   required: [true, 'Please Provide Password Confirm'],
+  //   validate: {
+  //     validator: function (el) {
+  //       return el === this.password
+  //     },
+  //     message: 'Password Do Not Match',
+  //   },
+  // },
   user_type: {
     type: String,
     enum: ['admin', 'user'],
@@ -37,8 +37,11 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined
   next()
 })
-userSchema.methods.correctPassword = async function (providedPassword, userPassword) {
-  return await bcrypt.compare(providedPassword,userPassword)
+userSchema.methods.correctPassword = async function (
+  providedPassword,
+  userPassword,
+) {
+  return await bcrypt.compare(providedPassword, userPassword)
 }
 const User = mongoose.model('User', userSchema)
 module.exports = User
