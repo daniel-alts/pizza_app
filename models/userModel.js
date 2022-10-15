6ae52f23f0df893fc4f38a65fdb467288e88ca6f
prosper <anosikeprosper2@gmail.com>
2022-10-15 06:58:25 -0700

    Updated user model with bcrypt for password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
@@ -6,7 +7,7 @@ const ObjectId = Schema.ObjectId;
 const UserSchema = new Schema({
   id: ObjectId,
 
-  name: {
+  username: {
     type: String,
     required: [true, "Please enter a name"],
   },
@@ -28,5 +29,23 @@ const UserSchema = new Schema({
     default: "user",
   },
 });
+
+// Before the user information is saved in the database, this function will be called,
+// you will get the plain text password, hash it, and store it.
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+// You will also need to make sure that the user trying to log in has the correct credentials. Add the following new method:
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
 
 module.exports = mongoose.model("User", UserSchema);
