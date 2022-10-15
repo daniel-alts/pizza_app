79471fabceda495c964081b1ed9c8348ca2ab7cd
Wilson Ezeibekwe <wezeibekwe675@gmail.com>
2022-10-15 11:58:58 +0100

    authentication, authorization, more routes and pagination

---
+++ db/database.js
@@ -0,0 +1,21 @@
+
+const moogoose = require('mongoose');
+require('dotenv').config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// connect to mongodb
+function connection() {
+    moogoose.connect(MONGODB_URI);
+
+    moogoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB successfully');
+    });
+
+    moogoose.connection.on('error', (err) => {
+        console.log('Error connecting to MongoDB', err);
+    })
+}
+
+
+module.exports = connection;
