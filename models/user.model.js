d0b4434be4593d963e6deb4ab7fd98337d26f8de
odelolajosh <oladapoodelola@gmail.com>
2022-10-15 06:15:12 +0100

    feat: add methods to user

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: {
@@ -9,7 +10,7 @@ const UserSchema = new Schema({
   },
   password: {
     type: String,
-    select: false,
+    // select: false,
     required: true
   },
   user_type: {
@@ -19,6 +20,33 @@ const UserSchema = new Schema({
 }, {
   timestamps: true
 });
+
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const user = this;
+  const isMatch = await bcrypt.compare(candidatePassword, user.password);
+  return isMatch;
+}
+
+UserSchema.methods.generateToken = async function () {
+  const user = this;
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+  return token;
+}
 
 const User = mongoose.model('User', UserSchema);
 
