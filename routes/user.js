a0c5c31492e87a31fe6716adb8b782b10884ea24
Eleos <Peaceaanu247@gmail.com>
2022-10-16 22:11:52 +0100

    jwt auth done

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,9 @@ const express = require('express')
 const UserRouter = express.Router()
 
 const userController = require("../controllers/userController")
-const { passwordAuth, adminAuth, userAuth } = require("../middleware/authMiddleware")
 
-
-UserRouter.post('/signup', userController.signup)
-UserRouter.get('/', [passwordAuth, adminAuth], userController.allUsers)
-UserRouter.patch('/:id', [passwordAuth], userController.updateUser)
-UserRouter.delete('/:id',[passwordAuth, adminAuth], userController.deleteUser)
+UserRouter.get('/', userController.allUsers)
+UserRouter.patch('/:id', userController.updateUser)
+UserRouter.delete('/:id', userController.deleteUser)
 
 module.exports = UserRouter 
