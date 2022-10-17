2418ff51a1320cef291bc5123d25a9ea61d59709
omotega <tomoyibo@gmail.com>
2022-10-17 02:38:00 +0100

    added passport authentication

---
+++ authentication/passport.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const User = require('../model/usermodel');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretorkey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user)
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+)
+
+
+passport.use(
+  "signup",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+
+        const username = req.body.username;
+        const user = await UserModel.create({ username, email, password });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
