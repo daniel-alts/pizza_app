ec7e2d100345d504334abb39578fbd3b8fa66934
omotega <tomoyibo@gmail.com>
2022-10-17 02:41:15 +0100

    updated the usermodel

--- model/usermodel.js
+++ model/usermodel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userschema = mongoose.Schema({
-    userName:{
+    username:{
         type: String,
         required: true
     },
@@ -17,5 +18,20 @@ const userschema = mongoose.Schema({
 },{
     timestamps:true,
 })
+
+userschema.pre('save',async(next) => {
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userschema.methods.comparePassword = async(candidatePassword, userPassword) =>{
+    return await bcrypt.compare(candidatePassword, userPassword)
+  }
+
+userschema.methods.isValidpassword = async(password) =>  {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+}
 
 module.exports = mongoose.model('User',userschema);
