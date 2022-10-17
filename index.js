05c44fba18b15934b6676f89cb0e64f9b1b8e63a
omotega <tomoyibo@gmail.com>
2022-10-17 02:44:24 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 const express = require('express');
-const moment = require('moment');
-const mongoose = require('mongoose');
+const passport = require('passport')
 const orderRouter = require('./routes/orderroute');
 const userRouter = require('./routes/userroute');
+const usermodel = require('./model/usermodel');
+require('dotenv').config();
+const { connectDb } = require('./config/db');
 
 
 const PORT = 3334
 
 const app = express()
 
+require('./authentication/passport');
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+
+
 
 
 app.get('/', (req, res) => {
-    return res.json({ status: true })
+	return res.json({ status: true })
 })
 
-app.use('/orders',orderRouter);
-app.use('/users',userRouter);
+app.use('/orders',passport.authenticate('jwt', { session: false }), orderRouter);
+app.use('/users', userRouter);
 
-
-mongoose.connect(process.env.MONGO_URI);
-
-mongoose.connection.on("connected", () => {
-	console.log("Connected to MongoDB Successfully");
-});
-
-mongoose.connection.on("error", (err) => {
-	console.log("An error occurred while connecting to MongoDB");
-	console.log(err);
-});
+connectDb();
 
 app.listen(PORT, () => {
-    console.log('Listening on port, ', PORT)
+	console.log('Listening on port, ', PORT)
 })
 
 module.exports = app;
