a76ac7f5c9d3a8e2a597bcfeacbc23c892ae6e6d
dhamolahedonist <dhamolahedonist>
2022-10-17 16:01:17 +0100

    orderRoute test updated

--- test/integration/orderRoute.test.js
+++ test/integration/orderRoute.test.js
@@ -6,7 +6,6 @@ const app = require("../../index");
 const orderModel = require("../../models/orderModel");
 const moment = require("moment");
 
-const orderId = "6348834cbd87bd6aebb8b075";
 const header = {
   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYzNDQ5N2E3NzBmNDRkZjE1N2E0MzI3MCIsImVtYWlsIjoiaGVkb25zaXRAZ21haWwuY29tIn0sImlhdCI6MTY2NTQzOTc1NX0.CSaJCb8FMLBnwTnSBfkWkH4ylPlwXKVr_RyU3h1vw5M`,
 };
@@ -18,38 +17,66 @@ const cleanup = async () => {
 
 // Get all orders
 describe("orderRoute", () => {
-  it.skip("GET /orders", async () => {
-    // await cleanup();
+  it("GET /orders", async () => {
+    await cleanup();
+    // create the orders :: Arrange
+    // creat order 1
+    const order1 = await orderModel.create({
+      items: [{ name: "dodo gizzard", price: 7500, size: "l", quantity: 5 }],
+      created_at: moment().toDate(),
+      state: 1,
+      total_price: 7500,
+    });
+    // create order 2
+    const order2 = await orderModel.create({
+      items: [
+        { name: "chicken periperi", price: 15000, size: "l", quantity: 3 },
+      ],
+      created_at: moment().toDate(),
+      state: 1,
+      total_price: 15000,
+    });
+
     const test = await request(app)
       .get("/orders")
       .set(header)
       .expect(200)
       .expect("content-type", /json/);
-    console.log(test.body.orders);
+    console.log(test.body.orders[1]);
 
     expect(test.body.status).toBe(true);
     expect(test.body.orders[0]._id).toBeDefined();
+    expect(order1.items[0].price).toBe(7500);
+    expect(order2.items[0].price).toBe(15000);
   });
 });
 
 // Get order by id
-it("GET /order/:orderId", async () => {
+it.skip("GET /order/:orderId", async () => {
   // Arrange
-  // await cleanup();
+  // Create the order
+  await cleanup();
+
+  const order = await orderModel.create({
+    items: [{ name: "dodo gizzard", price: 7500, size: "l", quantity: 5 }],
+    created_at: moment().toDate(),
+    state: 1,
+    total_price: 7500,
+  });
 
   // Act
   const test = await request(app)
-    .get(`/order/${orderId}`)
+    .get(`/order/${order._id}`)
     .set(header)
     .expect(200)
     .expect("content-type", /json/);
-  console.log(test.body.order);
+  console.log(test.body.order.items[0].name);
 
   // Assert
-  const order = await orderModel.findById(test.body.order._id);
   expect(test.body.status).toBe(true);
   expect(test.body.order).toBeDefined();
   expect(order._id.toString()).toBe(test.body.order._id);
+  expect(order.items[0].price).toBe(7500);
 });
 
 // Create order
