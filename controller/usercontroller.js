b1f6d18c3bc091db8464858ac3dc75f2ccb0d5b9
omotega <tomoyibo@gmail.com>
2022-10-17 02:40:29 +0100

    updated the usercontroller

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,16 +1,37 @@
+const passport = require('passport');
 const User = require('../model/usermodel');
+const { handleError,errorResponse,successResponse } = require('../utils/response')
 
-const createUser = async (req, res) => {
-    const { userName, password } = req.body;
-    if (!userName || !password) return res.status(400).json('please fill all fields');
-    const user = await User.create({
-        userName,
-        password,
-    })
-    if (!user) return res.status(400).json({ status: false, user: null })
-    return res.status(201).json({ status: true, user });
-}
+exports.signup = async (req, res, next) => {
+    res.json({
+      message: "Signup successful",
+      user: req.user,
+    });
+  
+};
 
-module.exports = {
-    createUser,
-}
+exports.login = async (req, res, next) => {
+    passport.authenticate("login", async (err, user, info) => {
+      try {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return next("Username or password is incorrect", 400);
+        }
+  
+        req.login(user, { session: false }, async (error) => {
+          if (error) return next(error);
+  
+          const body = { _id: user._id, email: user.email };
+          
+          const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
+  
+          return res.json({ token });
+        });
+      } catch (error) {
+        return next(error);
+      }
+    })(req, res, next);
+  };
+
