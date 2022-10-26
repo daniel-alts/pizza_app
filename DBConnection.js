ece31fc9d23afc4fd56fb267d99589cf870f243d
Muhammad <muhdibee@gmail.com>
2022-10-26 22:53:53 +0100

    Implimented user login.

--- DBConnection.js
+++ DBConnection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 
-const connectToMongoDB = () => {
+const connectToMongoDB = async () => {
 	mongoose.connect(MONGODB_CONNECTION_STRING);
 	mongoose.connection.on("connected", () => console.log("Connected to MongoDB Successfully."));
 	mongoose.connection.on("error", (error) => {
--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Create a connection to MongoDB.
-connectToMongoDB();
+try {
+	connectToMongoDB();
+} catch (err) {
+	console.log("error: ", err);
+	next();
+}
 
 app.use(express.json());
 app.use(logger("dev"));
@@ -26,23 +31,54 @@ app.post("/signup", async (req, res) => {
 	const salt = await bcrypt.genSalt(10);
 	const body = req.body;
 	const hash = await bcrypt.hash(body.password, salt);
-	const user = await UserModel.create({
-		username: body.username,
-		password: hash,
-		user_type: body.user_type,
-		created_at: new Date(),
-		updated_at: new Date(),
-	});
+	try {
+		const user = await UserModel.create({
+			username: body.username,
+			password: hash,
+			user_type: body.user_type,
+			created_at: new Date(),
+			updated_at: new Date(),
+		});
 
-	console.log("user", user);
-	const token = jwt.sign({ user_id: user._id, user_type: user.user_type }, JWT_SECRET, { expiresIn: "1h" });
-	res.status(200).json({ status: true, token });
+		// console.log("user", user);
+		const token = jwt.sign({ _id: user._id, username: user.username, user_type: user.user_type }, JWT_SECRET, { expiresIn: "1h" });
+		res.status(200).json({ status: true, token });
+	} catch (err) {
+		console.log("error: ", err);
+		res.status(400).send({ status: false, message: "An error ocurred" });
+	}
+});
 
-	// return res.status(400).send({ status: false, message: err });
+app.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
+		if (!username || !password) {
+			throw { status: 400, message: "username and password is required" };
+		}
+		const user = await UserModel.findOne({ username });
+		if (!user) {
+			throw { status: 400, message: "User not found." };
+		}
+		console.log("user: ", user);
+		const isValidPassword = await bcrypt.compare(password, user.password);
+		if (!isValidPassword) {
+			throw { status: 400, message: "Incorrect username or password." };
+		}
+		const token = jwt.sign({ _id: user._id, username: user.username, user_type: user.user_type }, JWT_SECRET, { expiresIn: "1h" });
+		res.status(200).json({ status: true, token });
+	} catch (err) {
+		next(err);
+	}
 });
 
 app.use("/orders", ordersRouter);
 app.use("/users", usersRouter);
+
+// Handle errors.
+app.use((err, req, res, next) => {
+	console.log("error: ", err);
+	res.status(err.status).json({ status: false, message: err.message });
+});
 
 app.listen(PORT, () => {
 	console.log("Listening on port, ", PORT);
