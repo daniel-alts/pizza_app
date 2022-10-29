2636c6c072535d54dec817e1521c865609a953c5
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-10-29 10:47:42 -0700

    Update order.route.test

--- test/integration/order.route.test.js
+++ test/integration/order.route.test.js
@@ -2,86 +2,65 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const moment = require('moment')
 const Order = require('../../model/orderModel')
+const User = require('../../model/usersModel')
 
 const app = require('../../app')
 
-
-let auth = 'wendy:12345'
+// ********************IMPLEMENT TEST*************************************//
 
 describe('Order Route', () => {
+    //add database connection hook - beforeAll()
+    let token;
+    beforeAll(async() => {
+        await mongoose.createConnection('mongodb://localhost:27017/test-db')
 
-// ***********CONNECT DATABASE ****************/
-    beforeAll(()=>{
-        mongoose.connect('mongodb://localhost:27017')
-    });
+        //sign up a new user
+        await User.create({
+            username: "John",
+            password: "123456"
+        })
 
-//********************TEST FOR ENDPOINTS IN ORDERS ROUTES***************//
-
-test(" /orders", async() => {
-
-    // ************* POST REQUEST *****************/
-    const body = {
-        "status":1,
-        "items": [{
-            "name":"pizza", "price":2500, "size":"m", "quantity":3
-        }]
-    }
-
-    const total_price = body.items.reduce((prev, curr) => {
-       
-        prev += (curr.price * curr.quantity)
-        return prev
-    }, 0);
-
-    const order = await Order.create({
-        items: body.items,
-        created_at: moment().format('MM/DD/YYYY'),
-        total_price
-    })
-    order.save()
-    
-   await request(app).post('api/orders/')
-    .set({Authorization : 'Basic ' + auth})
-    .send(order)
-   
-   
-    .expect((res) => {
-        res.body.status = body.status;
-        res.statusCode = 201
-        res.headers["content-type"] = 'application/json';
-    })
-    
-// **************** GET REQUEST *********************/ 
-
-    await request(app).get('/api/orders')
-    .set({Authorization : 'Basic ' + auth})
-    .expect('Content-Type', /json/)
-    .expect(200)
-
-// *************** GET BY ID REQUEST ****************/
-
-    await request(app).get('/api/orders/'+ order._id)
-    .set({Authorization : 'Basic ' + auth})
-    .expect('Content-Type', /json/)
-    .expect(200)
-    
-
-// *************** PATCH REQUEST ****************/
-
-    await request(app).patch('/api/orders/'+ order._id)
-        .set({Authorization : 'Basic ' + auth})
-        .expect('Content-Type', /json/)
+        //login user
+        const login = await request(app)
+        .post('/login')
+        .set('content-type', 'application/json')
         .send({
-            "state":2})
-        .expect(200)
-  
-})
+            username: "John",
+            password: "123456"
+        })
 
-
-// ***************CLOSE DB CONNECTION AFTER OPERATION ***********/
-    afterAll((done) =>{
-        mongoose.disconnect(done)
-    })
-    
+        token = login.body.message
         
+    })
+
+        afterEach(async () => {
+            await User.deleteMany();
+        })
+
+        afterAll(async () => {
+            await mongoose.connection.close();
+        })
+
+
+
+        it('should get all orders', async () => {
+            await Order.create({
+                state: 1,
+                items:{name: 'Pizzapie', price: 1000, quantity: 1, size: 'm'},
+                created_at: moment().format('MM/DD/YYYY'),
+                total_price: 900
+            })
+
+            const response = await request(app)
+            .get('/api/orders')
+            .set('content-type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            // .expect(200)
+            expect(response.status).toBe(200)
+            expect(response.body).toHaveProperty('orders')
+            expect(response.body).toHaveProperty('status', true)
+
+           
+        })
+   
 })
