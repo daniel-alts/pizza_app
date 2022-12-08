2ddd15f6eda484585b0f236cdf377608d8e0e6f3
Violacordis <ikemviolacordis@gmail.com>
2022-12-08 14:15:22 +0100

    user and order validation

---
+++ validators/order.validator.js
@@ -0,0 +1,29 @@
+const Joi = require("joi");
+
+const orderValidatorMiddleware = async (req, res, next) => {
+  const orderPayload = req.body;
+  try {
+    await orderValidator.validateAsync(orderPayload);
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(406).send(error.details[0].message);
+  }
+};
+
+const orderValidator = Joi.object({
+  created_at: Joi.date().default(Date.now()),
+  state: Joi.number().default(1),
+  total_cost: Joi.number(),
+  items: Joi.array().items(
+    Joi.object({
+      name: Joi.string().required(),
+      price: Joi.number().min(0).required(),
+      size: Joi.string().valid("m", "s", "l").required(),
+      quantity: Joi.number().required(),
+      cost: Joi.number(),
+    })
+  ),
+});
+
+module.exports = orderValidatorMiddleware;
---
+++ validators/user.validator.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+const userValidatorMiddleware = async (req, res, next) => {
+  const userPayload = req.body;
+  try {
+    await userValidator.validateAsync(userPayload);
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(406).send(error.details[0].message);
+  }
+};
+
+const userValidator = Joi.object({
+  username: Joi.string().required().messages({
+    "string.empty": "Please tell us your name",
+  }),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().required().min(8),
+  passwordConfirm: Joi.ref("password"),
+  user_type: Joi.string().valid("admin", "user").default("user"),
+  created_at: Joi.date().default(Date.now()),
+});
+
+module.exports = userValidatorMiddleware;
